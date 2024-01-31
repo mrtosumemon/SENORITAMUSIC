07f314c35e0fb129d62b9f1b3f3b7b77f2e8b56e
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-01-31 16:15:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/doraemon890/jarvisXmusic",
+    "https://github.com/doraemon890/ANNIE-X-MUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
