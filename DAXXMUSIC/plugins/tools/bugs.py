4f2922c5452a857628ee258a4770dd85e288c052
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-01-31 23:47:29 +0530

    Update bugs.py

--- DAXXMUSIC/plugins/tools/bugs.py
+++ DAXXMUSIC/plugins/tools/bugs.py
@@ -70,8 +70,8 @@ async def bugs(_, msg: Message):
                 ),
             )
             await app.send_photo(
-                -1001802990747,
-                photo="https://telegra.ph/file/f66e5843568d4b7f2a652.jpg",
+                -1002024677280,
+                photo="https://telegra.ph/file/2c6d1a6f78eba6199933a.jpg",
                 caption=f"{bug_report}",
                 reply_markup=InlineKeyboardMarkup(
                     [
