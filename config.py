a4be8d3a83e2cd8e39fb7fc386f0e44589a73c07
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-02-02 00:50:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -85,14 +85,14 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/51600957eaaf96ad308c7.jpg"
+    "START_IMG_URL", "https://telegra.ph/file/fdbffdb39d20374823466.jpg"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://telegra.ph/file/5f15712a84154fefd0e5b.jpg"
 )
 PLAYLIST_IMG_URL = "https://telegra.ph/file/facec75902f1b6b361a8b.jpg"
 STATS_IMG_URL = "https://telegra.ph/file/cd205021bf40f44ad78e4.jpg"
-TELEGRAM_AUDIO_URL = "https://telegra.ph/file/fdbffdb39d20374823466.jpg"
+TELEGRAM_AUDIO_URL = "https://telegra.ph/file/51600957eaaf96ad308c7.jpg"
 TELEGRAM_VIDEO_URL = "https://telegra.ph/file/782e4c83729ea24ab4d3d.jpg"
 STREAM_IMG_URL = "https://telegra.ph/file/f85e89871eb80c91f8a87.jpg"
 SOUNCLOUD_IMG_URL = "https://telegra.ph/file/7d9389f7a76acfd00d76a.jpg"
