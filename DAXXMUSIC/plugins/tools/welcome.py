b5cbc48b8c2480549918af2bf5ec0cc5d1be3efa
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-02-04 16:57:16 +0530

    Update welcome.py

--- DAXXMUSIC/plugins/tools/welcome.py
+++ DAXXMUSIC/plugins/tools/welcome.py
@@ -103,7 +103,7 @@ async def auto_state(_, message):
 @app.on_chat_member_updated(filters.group, group=-3)
 async def greet_group(_, member: ChatMemberUpdated):
     chat_id = member.chat.id
-    count = await app.get_chat_members_count(chat.id)
+    count = await app.get_chat_members_count(Chat)
     A = await wlcm.find_one(chat_id)  # Corrected this line
     if not A:
         return
