f09672c5ae9a38bc4ad81c0943b805824ae9e716
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-02-07 01:08:22 +0530

    Update speed.py

--- DAXXMUSIC/plugins/admins/speed.py
+++ DAXXMUSIC/plugins/admins/speed.py
@@ -22,15 +22,16 @@ checker = []
 async def playback(cli, message: Message, _, chat_id):
     playing = db.get(chat_id)
     if not playing:
-        return await message.reply_text(_["queue_2"])
+        return await app.send_message(message.chat.id, text=_["queue_2"])
     duration_seconds = int(playing[0]["seconds"])
     if duration_seconds == 0:
-        return await message.reply_text(_["admin_27"])
+        return await app.send_message(message.chat.id, text=_["admin_27"])
     file_path = playing[0]["file"]
     if "downloads" not in file_path:
-        return await message.reply_text(_["admin_27"])
+        return await app.send_message(message.chat.id, text=_["admin_27"])
     upl = speed_markup(_, chat_id)
-    return await message.reply_text(
+    return await app.send_message(
+        message.chat.id,
         text=_["admin_28"].format(app.mention),
         reply_markup=upl,
     )
@@ -56,42 +57,32 @@ async def del_back_playlist(client, CallbackQuery, _):
                     return await CallbackQuery.answer(_["admin_14"], show_alert=True)
     playing = db.get(chat_id)
     if not playing:
-        return await CallbackQuery.answer(_["queue_2"], show_alert=True)
+        return await callback_query.answer(_["queue_2"], show_alert=True)
     duration_seconds = int(playing[0]["seconds"])
     if duration_seconds == 0:
-        return await CallbackQuery.answer(_["admin_27"], show_alert=True)
+        return await callback_query.answer(_["admin_27"], show_alert=True)
     file_path = playing[0]["file"]
     if "downloads" not in file_path:
-        return await CallbackQuery.answer(_["admin_27"], show_alert=True)
-    checkspeed = (playing[0]).get("speed")
+        return await callback_query.answer(_["admin_27"], show_alert=True)
+    checkspeed = playing[0].get("speed")
     if checkspeed:
         if str(checkspeed) == str(speed):
             if str(speed) == str("1.0"):
-                return await CallbackQuery.answer(
-                    _["admin_29"],
-                    show_alert=True,
-                )
+                return await callback_query.answer(_["admin_29"], show_alert=True)
     else:
         if str(speed) == str("1.0"):
-            return await CallbackQuery.answer(
-                _["admin_29"],
-                show_alert=True,
-            )
+            return await callback_query.answer(_["admin_29"], show_alert=True)
     if chat_id in checker:
-        return await CallbackQuery.answer(
-            _["admin_30"],
-            show_alert=True,
-        )
+        return await callback_query.answer(_["admin_30"], show_alert=True)
     else:
         checker.append(chat_id)
     try:
-        await CallbackQuery.answer(
-            _["admin_31"],
-        )
+        await callback_query.answer(_["admin_31"])
     except:
         pass
-    mystic = await CallbackQuery.edit_message_text(
-        text=_["admin_32"].format(CallbackQuery.from_user.mention),
+    mystic = await app.send_message(
+        callback_query.message.chat.id,
+        text=_["admin_32"].format(callback_query.from_user.mention),
     )
     try:
         await DAXX.speedup_stream(
@@ -103,10 +94,15 @@ async def del_back_playlist(client, CallbackQuery, _):
     except:
         if chat_id in checker:
             checker.remove(chat_id)
-        return await mystic.edit_text(_["admin_33"], reply_markup=close_markup(_))
+        return await app.send_message(
+            callback_query.message.chat.id,
+            text=_["admin_33"],
+            reply_markup=close_markup(_),
+        )
     if chat_id in checker:
         checker.remove(chat_id)
-    await mystic.edit_text(
-        text=_["admin_34"].format(speed, CallbackQuery.from_user.mention),
+    await app.send_message(
+        callback_query.message.chat.id,
+        text=_["admin_34"].format(speed, callback_query.from_user.mention),
         reply_markup=close_markup(_),
-  )
+    )
