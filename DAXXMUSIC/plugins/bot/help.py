bc91c105267722d13e4f7708f8b9b02186e3f075
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-02-07 11:39:05 +0530

    Update help.py

--- DAXXMUSIC/plugins/bot/help.py
+++ DAXXMUSIC/plugins/bot/help.py
@@ -4,13 +4,13 @@ from pyrogram import filters, types
 from pyrogram.types import InlineKeyboardMarkup, Message
 
 from DAXXMUSIC import app
-from DAXXMUSIC.utils import help_pannel
+from DAXXMUSIC.utils import first_page, second_page
 from DAXXMUSIC.utils.database import get_lang
 from DAXXMUSIC.utils.decorators.language import LanguageStart, languageCB
 from DAXXMUSIC.utils.inline.help import help_back_markup, private_help_panel
 from config import BANNED_USERS, START_IMG_URL, SUPPORT_CHAT
 from strings import get_string, helpers
-
+from DAXXMUSIC.misc import SUDOERS
 
 @app.on_message(filters.command(["help"]) & filters.private & ~BANNED_USERS)
 @app.on_callback_query(filters.regex("settings_back_helper") & ~BANNED_USERS)
@@ -26,7 +26,7 @@ async def helper_private(
         chat_id = update.message.chat.id
         language = await get_lang(chat_id)
         _ = get_string(language)
-        keyboard = help_pannel(_, True)
+        keyboard = first_page(_)
         await update.edit_message_text(
             _["help_1"].format(SUPPORT_CHAT), reply_markup=keyboard
         )
@@ -37,7 +37,7 @@ async def helper_private(
             pass
         language = await get_lang(update.chat.id)
         _ = get_string(language)
-        keyboard = help_pannel(_)
+        keyboard = first_page(_)
         await update.reply_photo(
             photo=START_IMG_URL,
             caption=_["help_1"].format(SUPPORT_CHAT),
@@ -130,3 +130,5 @@ async def first_pagexx(client, CallbackQuery, _):
     except:
         return
 
+
+        
